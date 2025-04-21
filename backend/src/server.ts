5baf851eecad48262a2aab87e8391d40cc0b7ede
James Lu <z5417290@student.unsw.edu.au>
2025-04-21 16:58:15 +1000

    Fixed merge issues

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,38 +1,3 @@
-<<<<<<< Updated upstream
-import cors from 'cors';
-import express, { Request, Response } from 'express';
-import authRoutes from './auth';
-import businessRoutes from './business';
-import { BACKEND_PORT } from './config';
-import { initializeDataStore, resetDataStore } from './dataStore';
-import invoiceRoutes from './invoice';
-
-// Initialise app
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// API Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/business', businessRoutes);
-app.use('/api/invoice', invoiceRoutes);
-
-// Testing Endpoint
-app.delete('/api/testing/clear-users', (req: Request, res: Response) => {
-  resetDataStore();
-  res.json({ success: true });
-});
-
-// Initialising temporary data store
-initializeDataStore();
-
-// Start Server
-app.listen(BACKEND_PORT, () => {
-  console.log(`Server running on ${BACKEND_PORT}`);
-});
-
-export default app;
-=======
 import cors from 'cors';
 import express, { Request, Response } from 'express';
 import authRoutes from './auth';
@@ -64,4 +29,3 @@ app.listen(BACKEND_PORT, () => {
 });
 
 export default app;
->>>>>>> Stashed changes
